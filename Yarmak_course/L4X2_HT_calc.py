f2ce82aa8cdea75e93f37be7c9503cd909e4629b
Sergii Khlivnenko <s.khlivnenko@gmail.com>
2018-07-16 19:39:05 +0300

    Calc update (not work)

--- Yarmak_course/L4X2_HT_calc.py
+++ Yarmak_course/L4X2_HT_calc.py
@@ -1,3 +1,31 @@
 #write a calculator with console input, that works with +,-,*,/
 
-#TBD
+
+expression = raw_input("Input expression")
+#check input: print "{}".format(expression)
+
+operators = ["+","-","*","/"]
+
+#parse first number until meet operator
+i=0
+element1 = ""
+
+
+for i in range(0,len(expression)):
+    for v in range(0, len(operators)):
+        if expression[i] == operators[v]: break
+        break
+    element1 += expression[i]
+
+
+print element1
+
+
+
+#parse operator
+#parse next number
+#describe functions for every operator
+#start calculation
+#print result
+
+
